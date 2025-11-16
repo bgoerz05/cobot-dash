ce49e7ec9acc667da5ddddb93e30c8e515728c2e
bgoerz05 <54515340+bgoerz05@users.noreply.github.com>
2025-11-15 22:26:15 -0500

    Add chessboard

--- cobot_dashboard/lib/panels/board_panel.dart
+++ cobot_dashboard/lib/panels/board_panel.dart
@@ -1,3 +1,7 @@
+import 'dart:math';
+
+import 'package:chessground/chessground.dart';
+import 'package:dartchess/dartchess.dart';
 import 'package:flutter/material.dart';
 
 class BoardPanel extends StatelessWidget {
@@ -8,7 +12,17 @@ class BoardPanel extends StatelessWidget {
     return Card(
       child: Scaffold(
         appBar: AppBar(title: Text("Game Board")),
-        body: const Placeholder(),
+        body: Center(
+          child: LayoutBuilder(
+            builder: (context, constraints) {
+              return Chessboard.fixed(
+                size: min(constraints.maxHeight, constraints.maxWidth),
+                orientation: Side.white,
+                fen: 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR',
+              );
+            },
+          ),
+        ),
       ),
     );
   }
