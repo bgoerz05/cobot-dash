c1147a1e2f25f24ae8285d71b36fa604aab45fbb
bgoerz05 <54515340+bgoerz05@users.noreply.github.com>
2025-11-19 15:25:24 -0500

    Prepare code structure for bloc

---
+++ cobot_dashboard/lib/features/home.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:cobot_dashboard/features/board/board_panel.dart';
+import 'package:cobot_dashboard/features/clock/clock_panel.dart';
+import 'package:cobot_dashboard/features/controls/cobot_control.dart';
+import 'package:cobot_dashboard/features/move_log/move_log.dart';
+
+class DashHomePage extends StatelessWidget {
+  const DashHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(title)),
+      body: Row(
+        children: [
+          Expanded(
+            child: Column(
+              children: [
+                Expanded(flex: 2, child: BoardPanel()),
+                Expanded(flex: 1, child: MoveLogPanel()),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Column(
+              children: [
+                Expanded(flex: 2, child: CobotControlPanel()),
+                Expanded(flex: 1, child: ClockPanel()),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- cobot_dashboard/lib/main.dart
+++ cobot_dashboard/lib/main.dart
@@ -1,7 +1,4 @@
-import 'package:cobot_dashboard/panels/board_panel.dart';
-import 'package:cobot_dashboard/panels/clock_panel.dart';
-import 'package:cobot_dashboard/panels/cobot_control.dart';
-import 'package:cobot_dashboard/panels/move_log.dart';
+import 'package:cobot_dashboard/features/home.dart';
 import 'package:cobot_dashboard/theme.dart';
 import 'package:flutter/material.dart';
 
@@ -18,47 +15,6 @@ class DashboardApp extends StatelessWidget {
       title: 'Cobot Control',
       theme: dashTheme,
       home: const DashHomePage(title: 'Dashboard'),
-    );
-  }
-}
-
-class DashHomePage extends StatelessWidget {
-  const DashHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text(title)),
-      // body: GridView.count(
-      //   crossAxisCount: 2,
-      //   children: [
-      //     BoardPanel(),
-      //     CobotControlPanel(),
-      //     MoveLogPanel(),
-      //     ClockPanel(),
-      //   ],
-      // ),
-      body: Row(
-        children: [
-          Expanded(
-            child: Column(
-              children: [
-                Expanded(flex: 2, child: BoardPanel()),
-                Expanded(flex: 1, child: MoveLogPanel()),
-              ],
-            ),
-          ),
-          Expanded(
-            child: Column(
-              children: [
-                Expanded(flex: 2, child: CobotControlPanel()),
-                Expanded(flex: 1, child: ClockPanel()),
-              ],
-            ),
-          ),
-        ],
-      ),
     );
   }
 }
