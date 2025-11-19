cd7aa692126c04adc8a55153c3e0f021f8f4d432
bgoerz05 <54515340+bgoerz05@users.noreply.github.com>
2025-11-19 15:16:39 -0500

    Add visuals to clock panel.

--- cobot_dashboard/lib/panels/clock_panel.dart
+++ cobot_dashboard/lib/panels/clock_panel.dart
@@ -8,7 +8,29 @@ class ClockPanel extends StatelessWidget {
     return Card(
       child: Scaffold(
         appBar: AppBar(title: const Text("Game Clock")),
-        body: const Placeholder(),
+        body: Row(
+          children: [
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  border: BoxBorder.all(color: Colors.yellow, width: 8),
+                  color: Colors.white,
+                ),
+                child: Center(
+                  child: Text("5:00", style: TextStyle(fontSize: 44)),
+                ),
+              ),
+            ),
+            Expanded(
+              child: Center(
+                child: Text(
+                  "5:00",
+                  style: TextStyle(color: Colors.white, fontSize: 44),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
