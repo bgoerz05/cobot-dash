cde34af85f6951d8283fc3403b759e07f2bb51e2
bgoerz05 <54515340+bgoerz05@users.noreply.github.com>
2025-11-19 19:00:10 -0500

    Begin filling out clock bloc.

--- cobot_dashboard/lib/features/clock/clock_panel.dart
+++ cobot_dashboard/lib/features/clock/clock_panel.dart
@@ -1,4 +1,7 @@
+import 'package:cobot_dashboard/features/clock/clock_bloc.dart';
+import 'package:cobot_dashboard/features/clock/clock_state.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class ClockPanel extends StatelessWidget {
   const ClockPanel({super.key});
@@ -8,30 +11,50 @@ class ClockPanel extends StatelessWidget {
     return Card(
       child: Scaffold(
         appBar: AppBar(title: const Text("Game Clock")),
-        body: Row(
-          children: [
-            Expanded(
-              child: Container(
-                decoration: BoxDecoration(
-                  border: BoxBorder.all(color: Colors.yellow, width: 8),
-                  color: Colors.white,
-                ),
-                child: Center(
-                  child: Text("5:00", style: TextStyle(fontSize: 44)),
-                ),
-              ),
-            ),
-            Expanded(
-              child: Center(
-                child: Text(
-                  "5:00",
-                  style: TextStyle(color: Colors.white, fontSize: 44),
-                ),
-              ),
-            ),
-          ],
+        body: BlocProvider(
+          create: (context) => ClockBloc(),
+          child: BlocBuilder<ClockBloc, ClockState>(
+            builder: (context, state) {
+              return Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      decoration: BoxDecoration(
+                        border: BoxBorder.all(color: Colors.yellow, width: 8),
+                        color: Colors.white,
+                      ),
+                      child: Center(
+                        child: Text(
+                          getFormattedTime(state.whiteTimeLeft),
+                          style: TextStyle(fontSize: 44),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Center(
+                      child: Text(
+                        getFormattedTime(state.blackTimeLeft),
+                        style: TextStyle(color: Colors.white, fontSize: 44),
+                      ),
+                    ),
+                  ),
+                ],
+              );
+            },
+          ),
         ),
       ),
     );
+  }
+
+  String getFormattedTime(Duration duration) {
+    int minutes = duration.inMinutes;
+    int seconds = duration.inSeconds % 60;
+    if (seconds < 10) {
+      return "$minutes:0$seconds";
+    } else {
+      return "$minutes:$seconds";
+    }
   }
 }
