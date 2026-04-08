8314bbabdc570bc29d7930e135b05251bd4d065b
bgoerz05 <54515340+bgoerz05@users.noreply.github.com>
2026-04-08 19:41:34 -0400

    Clock bloc tests

--- cobot_dashboard/test/widget/clock_test.dart
+++ cobot_dashboard/test/widget/clock_test.dart
@@ -1,5 +1,17 @@
+import 'package:cobot_dashboard/features/clock/clock_panel.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  testWidgets('Clock Widget Test', (WidgetTester tester) async {});
+  late Widget widget;
+
+  setUp(() {
+    widget = MaterialApp(home: Scaffold(body: ClockPanel()));
+  });
+  testWidgets('Clock Widget Test', (WidgetTester tester) async {
+    await tester.pumpWidget(widget);
+
+    // Expect both clocks to read 5:00 by default.
+    expect(find.text("5:00"), findsExactly(2));
+  });
 }
