785d533da552e9fb35ca82ce94760d9c04c526a4
cobot-pc <n/a>
2026-04-15 12:08:13 -0400

    Basic websocket setup

---
+++ cobot_dashboard/lib/services/websocket_service.dart
@@ -0,0 +1,11 @@
+import 'package:web_socket_channel/web_socket_channel.dart';
+
+class WebsocketService {
+  static WebsocketService? _serviceInstance;
+  WebsocketService._();
+
+  static WebsocketService get websocketService => _serviceInstance ??= WebsocketService._();
+
+  final WebSocketChannel channel = WebSocketChannel.connect(Uri.parse("ws://localhost:8080"));
+  
+}
